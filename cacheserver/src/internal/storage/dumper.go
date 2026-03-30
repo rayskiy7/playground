ae9340f8de4089886e3d353d66ef1af3e997e666
rayskiy7 <rayskiy7@mail.ru>
2026-03-30 10:16:03 +0300

    add dump file write/load

--- cacheserver/src/internal/storage/dumper.go
+++ cacheserver/src/internal/storage/dumper.go
@@ -1,7 +1,10 @@
 package storage
 
 import (
+	"bufio"
 	"context"
+	"log"
+	"os"
 	"time"
 
 	"github.com/rayskiy7/playground/cacheserver/pkg/cache"
@@ -54,18 +57,79 @@ func (d *Dumper) run(ctx context.Context) {
 }
 
 func (d *Dumper) doDump(ctx context.Context) {
-	data := d.cache.Dump(ctx)
-	// open file...
+	if d.path == "" {
+		log.Printf("No env DUMPER_PATH set. Skip Dump\n")
+		return
+	}
+
+	tmpPath := d.path + ".tmp"
+	file, err := os.OpenFile(tmpPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Printf("Error opening file for dump: %v\n", err)
+		return
+	}
+
+	writer := bufio.NewWriter(file)
+	success := false
+
+	defer func() {
+		file.Close()
+		if success {
+			_ = os.Rename(tmpPath, d.path)
+		} else {
+			_ = os.Remove(tmpPath)
+		}
+	}()
+
+	dataCh := d.cache.Dump(ctx)
 
 	for {
 		select {
-		case chunk, ok := <-data:
+		case chunk, ok := <-dataCh:
 			if !ok {
+				if err := writer.Flush(); err != nil {
+					log.Printf("Error Flush()! Dump not stored: %v\n", err)
+					return
+				}
+				if err := file.Sync(); err != nil {
+					log.Printf("Error Sync()! Dump not stored: %v\n", err)
+					return
+				}
+				success = true
 				return
 			}
-			_ = chunk // TODO: write chunk to file
+
+			if _, err := writer.Write(chunk); err != nil {
+				log.Printf("Error writing 1 chunk to file: %v\n", err)
+				return
+			}
 		case <-ctx.Done():
 			return
 		}
+	}
+}
+
+func (d *Dumper) LoadImage(ctx context.Context, c cache.Cache) {
+	if d.path == "" {
+		log.Printf("No env DUMPER_PATH set. Skip LoadImage\n")
+		return
+	}
+
+	file, err := os.Open(d.path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Printf("info: no dump file on start. Strating without dump!\n")
+			return
+		}
+		log.Printf("Error opening file for load: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	if err := c.FillFromImage(ctx, reader); err != nil {
+		log.Printf("Error filling cache from image: %v\n", err)
+		return
 	}
 }
